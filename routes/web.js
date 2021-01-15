408ae5ec68dea7947e7db378e1bc6d7bf89c9319
ElGalactico <ugbcaballero@gmail.com>
2021-01-15 11:53:54 -0600

    Login screen

--- routes/web.js
+++ routes/web.js
@@ -9,4 +9,8 @@ router.get("/",(req,res,next) => {
     res.render('main')
 })
 
+router.get('/login',(req,res,next) => {
+    res.render('login',{layout : 'loginLayout'})
+})
+
 module.exports = {webRoute}
